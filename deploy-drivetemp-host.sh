c7c323f5f3c746f0cabe0a94a0b15c57f792ae8c
rico <rico@rmhome.co.uk>
2025-05-01 14:00:21 +0100

    add service and installer

---
+++ deploy-drivetemp-host.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/bash
+
+systemctl stop drivetemp-drive-host-0.service
+systemctl stop drivetemp-drive-host-1.service
+
+systemctl disable drivetemp-drive-host-0.service
+systemctl disable drivetemp-drive-host-1.service
+
+git pull
+
+rm /etc/systemd/system/drivetemp-drive-host-0.service
+rm /etc/systemd/system/drivetemp-drive-host-1.service
+
+cp drivetemp-drive-host-0.service /etc/systemd/system/
+cp drivetemp-drive-host-1.service /etc/systemd/system/
+
+systemctl enable drivetemp-drive-host-0.service
+systemctl enable drivetemp-drive-host-1.service
+
+systemctl start drivetemp-drive-host-0.service
+systemctl start drivetemp-drive-host-1.service
+
